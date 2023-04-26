aa70ffa2801dd13f94548e8045dea6ca34b0c850
Neku <akpotohwoo@gmail.com>
2023-04-26 16:50:33 +0100

    Added Projects

--- portfolio/portfolio/src/components/Hero/Hero.js
+++ portfolio/portfolio/src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi <br/>
+        I'm Chukwuneku
+      </SectionTitle>
+      <SectionText>
+        Software Developer, Tech Enthusiast and current Computer Science student at Babcock University
+      </SectionText>
+      <Button onClick={() => window.location="https://github.com/nekumartins"}> About </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
