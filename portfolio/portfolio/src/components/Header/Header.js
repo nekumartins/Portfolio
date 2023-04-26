7aa9cd98b7f7e21f0562a06b3295737259ed5032
Neku <akpotohwoo@gmail.com>
2023-04-26 09:24:33 +0100

    Added Portfolio header

--- portfolio/portfolio/src/components/Header/Header.js
+++ portfolio/portfolio/src/components/Header/Header.js
@@ -6,9 +6,17 @@ import { DiCssdeck } from 'react-icons/di';
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+      <Div1>
+      <Link legacyBehavior href="/" passHref>
+      <a style={{ display: "flex", alignItems: "center", color: 'white'}}>
+      <DiCssdeck size="3rem" /> <span>Portfolio</span> 
+      </a>
+      </Link>
+
+
+      </Div1>
+  </Container>
 );
 
 export default Header;
