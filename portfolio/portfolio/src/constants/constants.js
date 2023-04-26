9e968ba938b97549b3a1ae856eb2fcf8cdae4cc1
Neku <akpotohwoo@gmail.com>
2023-04-26 16:56:03 +0100

    Added Links to sources

--- portfolio/portfolio/src/constants/constants.js
+++ portfolio/portfolio/src/constants/constants.js
@@ -4,8 +4,8 @@ export const projects = [
     description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
       image: '/images/1.png',
       tags: ['Discord', 'Python'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    source: 'https://github.com/nekumartins/Discord',
+    visit: 'https://github.com/nekumartins/Discord',
     id: 0,
   },
   {
@@ -13,8 +13,8 @@ export const projects = [
     description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
     image: '/images/2.png',
     tags: ['Python', 'TensorFlow'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    source: 'https://github.com/nekumartins/AI-With-Python/tree/main/tictactoe',
+    visit: 'https://github.com/nekumartins/AI-With-Python/tree/main/tictactoe',
     id: 1,
   },
   {
@@ -22,8 +22,8 @@ export const projects = [
     description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
       image: '/images/tux.png',
       tags: ['Python', 'cowsay'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    source: 'https://github.com/nekumartins/cowsay',
+    visit: 'https://github.com/nekumartins/cowsay',
     id: 2,
   },
   {
@@ -31,7 +31,7 @@ export const projects = [
     description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
     image: '/images/nurse.png',
     tags: ['Flask', 'Python', 'HTML'],
-    visit: 'https://google.com',
+    visit: 'https://github.com/babcock-campus-hackathon-2023/Candela',
     id: 3,
   },
 ];
